7920dfba45d48ffd21b4e3301d53ff6a57595f0e
jckstllrd <jackstallard17@gmail.com>
2024-09-15 17:51:40 +0100

    Complete playRound function

--- index.js
+++ index.js
@@ -13,4 +13,46 @@ function getComputerChoice() {
   return compChoice;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let humanChoice = prompt("Give me answers");
+
+  return humanChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "rock" && computerChoice == "paper") {
+    computerScore++;
+    console.log("Human: " + humanScore + "Computer: " + computerScore);
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    humanScore++;
+    console.log("Human: " + humanScore + "Computer: " + computerScore);
+  } else if (humanChoice == "rock" && computerChoice == "rock") {
+    console.log("Draw");
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    humanScore++;
+    console.log("Human: " + humanScore + "Computer: " + computerScore);
+  } else if (humanChoice == "paper" && computerChoice == "scissors") {
+    computerScore++;
+    console.log("Human: " + humanScore + "Computer: " + computerScore);
+  } else if (humanChoice == "paper" && computerChoice == "paper") {
+    console.log("Draw");
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    computerChoice++;
+    console.log("Human: " + humanScore + "Computer: " + computerScore);
+  } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+    humanScore++;
+    console.log("Draw");
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    humanScore++;
+    console.log("Human: " + humanScore + "Computer: " + computerScore);
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
