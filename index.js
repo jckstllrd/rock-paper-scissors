6a1933164a88d0c5c027deb6763fb2a89ff1fe2f
jckstllrd <jackstallard17@gmail.com>
2024-09-15 16:18:43 +0100

    Complete getComputerChoice function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getComputerChoice() {
+  let randNum = Math.floor(Math.random() * (4 - 1)) + 1;
+  let compChoice;
+  if (randNum == 1) {
+    compChoice = "rock";
+  } else if (randNum == 2) {
+    compChoice = "paper";
+  } else if (randNum == 3) {
+    compChoice = "scissors";
+  }
+  console.log(randNum);
 
+  return compChoice;
+}
+
+console.log(getComputerChoice());
